6d7965f062f49fed5e712299fa493a4fa46a9699
rahhal <saidbnrahhal@gmail.com>
2018-03-02 00:44:43 +0100

    clean front conf

--- src/main/webapp/app/app.js
+++ src/main/webapp/app/app.js
@@ -2,11 +2,6 @@
     'use strict';
     
     var app = angular.module('sambaApp',['users.module','ui.router','ngStorage']);
-
-    app.constant('urls', {
-        BASE: 'http://localhost:8080/SpringBootCRUDApp',
-        USER_SERVICE_API : 'http://localhost:8080/SpringBootCRUDApp/api/user/'
-    });
     
 })();
 
